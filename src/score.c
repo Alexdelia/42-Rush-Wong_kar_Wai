fd52ccfe6d8624ba62335e399af157e4580e3cf1
Alexdelia <alexandre.delille.57@gmail.com>
2022-03-18 17:00:29 +0100

    fix

--- src/score.c
+++ src/score.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 15:55:46 by adelille          #+#    #+#             */
-/*   Updated: 2022/03/18 16:51:53 by adelille         ###   ########.fr       */
+/*   Updated: 2022/03/18 17:00:15 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,14 @@ static bool	write_score(t_env *e, char *pseudo, const int fd)
 	ft_putstr_fd(tmp, fd);
 	free(tmp);
 	ft_putstr_fd(" ", fd);
-	printw(" .");
-	refresh();
 	tmp = ft_sttoa(e->top);
 	if (!tmp)
 		return (false);
 	ft_putstr_fd(tmp, fd);
 	free(tmp);
 	ft_putstr_fd(" ", fd);
-	printw(" .");
-	refresh();
 	ft_putstr_fd(pseudo, fd);
 	ft_putstr_fd("\n", fd);
-	attrset(COLOR_PAIR(CP_GREEN));
-	printw("✓");
-	attrset(A_NORMAL);
-	refresh();
-	getch(); //
 	return (true);
 }
 
@@ -51,6 +42,7 @@ bool	save_score(t_env *e)
 	clear();
 	mvprintw(1, (e->col - (ft_strlen("SCORE: ") + ft_stlen(e->score))) / 2,
 		"SCORE: %ld\n", e->score);
+	curs_set(1);
 	pmw(e, "do you want to save your score [y/n]");
 	e->key = getch();
 	if (e->key != 'y' && e->key != 'Y')
@@ -60,7 +52,7 @@ bool	save_score(t_env *e)
 	pmw(e, "What is your pseudo: ");
 	e->key = getch();
 	i = 0;
-	while (i < 100 && e->key != KEY_ENTER)
+	while (i < 100 && e->key != '\n')
 	{
 		if (i > 0 && e->key == KEY_BACKSPACE)
 			i--;
@@ -73,8 +65,6 @@ bool	save_score(t_env *e)
 			break ;
 		e->key = getch();
 	}
-	printw(" .");
-	refresh();
 	fd = open(SCORE_PATH, O_CREAT | O_RDWR | O_APPEND, 0664);
 	if (fd < 0)
 		return (false);
